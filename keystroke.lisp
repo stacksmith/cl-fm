07528e3dd06baa547a7cb5eafb877a1075561b75
stacksmith <fpgasm@apple2.x10.mx>
2016-08-31 15:26:09 -0700

    key mod is an integer map

--- keystroke.lisp
+++ keystroke.lisp
@@ -22,12 +22,19 @@
   (:release-mask #.(ash 1 30))
   (:modifier-mask #x5c001fff))
 |#
-(defconstant MOD-CONTROL-MASK (ash 1 0))
-(defconstant MOD-META-MASK    (ash 1 1)) 
-(defconstant MOD-ALT-MASK     (ash 1 2)) 
-(defconstant MOD-SHIFT-MASK   (ash 1 3)) 
-(defconstant MOD-SUPER-MASK   (ash 1 4)) 
-(defconstant MOD-HYPER-MASK   (ash 1 5))
+(defconstant MOD-CONTROL-BIT 0)
+(defconstant MOD-META-BIT 1)
+(defconstant MOD-ALT-BIT 2)
+(defconstant MOD-SHIFT-BIT 3)
+(defconstant MOD-SUPER-BIT 4)
+(defconstant MOD-HYPER-BIT 5)
+
+(defconstant MOD-CONTROL-MASK (ash 1 MOD-CONTROL-BIT) )
+(defconstant MOD-META-MASK    (ash 1 MOD-META-BIT) ) 
+(defconstant MOD-ALT-MASK     (ash 1 MOD-ALT-BIT) ) 
+(defconstant MOD-SHIFT-MASK   (ash 1 MOD-SHIFT-BIT) ) 
+(defconstant MOD-SUPER-MASK   (ash 1 MOD-SUPER-BIT) ) 
+(defconstant MOD-HYPER-MASK   (ash 1 MOD-HYPER-BIT) )
 
 
 (defstruct key keysym mod)
@@ -40,21 +47,21 @@
   (and (< keysym char-code-limit)
        (code-char keysym)))
 
-
-
 (defun print-key (key)
   "Convert a key into a string representation"
   (let ((mod (key-mod key)))
     (concatenate 'string
-		 (when (logand mod mod-control-mask)"C-")
-		 (when (logand mod mod-meta-mask)  "M-")
-		 (when (logand mod mod-alt-mask)   "A-")
-		 (when (logand mod mod-shift-mask) "S-")
-		 (when (logand mod mod-super-mask) "s-")
-		 (when (logand mod mod-hyper-mask) "H-")
+		 (when (logbitp mod-control-bit mod) "C-")
+		 (when (logbitp mod-meta-bit    mod) "M-")
+		 (when (logbitp mod-alt-bit     mod) "A-")
+		 (when (logbitp mod-shift-bit   mod) "S-")
+		 (when (logbitp mod-super-bit   mod) "s-")
+		 (when (logbitp mod-hyper-bit   mod) "H-")
 		 (keysym->keysym-name (key-keysym key)))))
-
-(defun gtk-modifiers (gtk-modifiers)
+;;;
+;;; We only care about control and meta (alt key).
+;;; Shift is already processed for us.
+(defun gtk->modmap (gtk-modifiers)
   "scan the gtk-modifier list and create a mod bitmap"
   (let ((modmap 0))
     (dolist (modifier gtk-modifiers)
@@ -69,9 +76,7 @@
   (let ((keysym (gdk-event-key-keyval event)))
     (unless (member keysym *modifier-keys*) ;skip modifier keypresses
       (format t "~A~%" 
-	      (print-key  (make-key :keysym keysym :mod (gtk-modifiers (gdk-event-key-state event)))))
-       ))
-  
+	      (print-key  (make-key :keysym keysym :mod (gtk->modmap (gdk-event-key-state event)))))))  
   t)
 
 
