c916cf5ec506c1583e0c2cb6e510366a42c230c0
stacksmith <fpgasm@apple2.x10.mx>
2016-10-01 14:59:43 -0700

    file action RENAME

--- file-actions.lisp
+++ file-actions.lisp
@@ -7,4 +7,6 @@
     ((string= action "COPY") (format t "COPY ~A" files))
     ((string= action "MOVE") (format t "MOVE ~A" files))
     ((string= action "LINK") (format t "LINK ~A" files))
-    ((string= action "ASK" ) (format t "ASK  ~A" files))))
+    ((string= action "ASK" ) (format t "ASK  ~A" files))
+    ((string= action "RENAME") (format t "RENAME ~A" files))
+    ))
